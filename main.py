0404b96b03391480e865bbc077afccde6b81f01f
Miles Steele <miles@milessteele.com>
2020-03-14 13:50:12 -0400

    sort lists

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from nanoleaf.nanoleaf import setup
 from nanoleaf.nanoleaf import Aurora
 import requests
 from pprint import pprint
+import time
 from secrets import ip, port, auth_token
 
 # ips = setup.find_auroras()
@@ -27,7 +28,6 @@ def api_call(method, endpoint, data=None):
 
 # Takes a map from panel ID to a list of frames.
 # Each frame is a list [r, g, b, t].
-# Frames other than the first for each panel seem to have no effect.
 def anim_data(data):
   # https://forum.nanoleaf.me/docs/openapi#_sh5xwlxaz1pa
   res = f"{len(data)}"
@@ -37,6 +37,14 @@ def anim_data(data):
       w = 0
       res += f" {r} {g} {b} {w} {t}"
   return res
+
+def get_panel_ids():
+  return [panel['panelId'] for panel in api_call("GET", "panelLayout/layout")['positionData']]
+
+# panel_ids_raw = get_panel_ids()
+panel_ids_raw = [251, 126, 87, 209, 8, 203, 48, 122, 162, 191]
+panel_ids_reorder = [10, 1, 2, 9, 3, 4, 5, 6, 8, 7]
+panel_ids = [x[0] for x in sorted(zip(panel_ids_raw, panel_ids_reorder), key=lambda x: x[1])]
 
 # pprint(api_call("GET", ""))
 # pprint(api_call("GET", "state"))
@@ -60,11 +68,31 @@ def anim_data(data):
 #   "loop": False,
 # }}))
 
-pprint(api_call("PUT", "effects", {"write": {
-  "command": "display",
-  "animType": "static",
-  "animData": anim_data({
-    162: [(255, 255, 0, 20)],
-  }),
-  "loop": False,
-}}))
+# pprint(api_call("PUT", "effects", {"write": {
+#   "command": "display",
+#   "animType": "custom",
+#   "animData": anim_data({
+#     162: [(255, 255, 255, 5), (255, 0, 0, 10)],
+#   }),
+#   "loop": True,
+# }}))
+
+print(panel_ids)
+for panel_id in panel_ids:
+  api_call("PUT", "effects", {"write": {
+    "command": "display",
+    "animType": "custom",
+    "animData": anim_data({
+      panel_id: [(255, 255, 255, 2), (255, 0, 0, 4)],
+    }),
+    "loop": True,
+  }})
+  time.sleep(1)
+  api_call("PUT", "effects", {"write": {
+    "command": "display",
+    "animType": "custom",
+    "animData": anim_data({
+      panel_id: [(0, 0, 0, 5)],
+    }),
+    "loop": False,
+  }})
